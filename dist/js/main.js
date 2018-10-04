ef26c60a13dcd885806418b3bd8012eba4fc2124
Jack O'Connor <jack.oconnor@live.com>
2018-10-04 17:53:02 +0100

    Menu Overlay for Desktop

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,17 +13,17 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add("close");
+        menu.classList.add("show");
         menuNav.classList.add("show");
-        menuBtn.classList.add("show");
         menuBranding.classList.add("show");
         navItems.forEach(item => item.classList.add('show'));
 
-        // Set Menu State
+        // Reset Menu State
         showMenu = true;
     } else {
         menuBtn.classList.remove("close");
+        menu.classList.remove("show");
         menuNav.classList.remove("show");
-        menuBtn.classList.remove("show");
         menuBranding.classList.remove("show");
         navItems.forEach(item => item.classList.remove('show'));
 
